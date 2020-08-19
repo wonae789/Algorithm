6e7c1e669384d08658eaa23aff2ed81031b9d5a2
hoon <wonae789@naver.com>
2020-08-19 13:54:11 +0900

    sort

---
+++ Algorithm/sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+	int a[10] = { 14,242,36,124,125,66,74,28,94,110 };
+	sort(a, a + 10); // 배열 시작주소, 마지막주소 +1
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << ' ';
+	}
+
+	return 0;
+}
